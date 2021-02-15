05c84aaae8c60954f150b09f75653847652b35e6
Jonathan Reeve <jon.reeve@gmail.com>
2021-02-14 20:20:33 -0500

    add new notes

---
+++ .dir-locals.el
@@ -0,0 +1,2 @@
+((nil . ((org-roam-directory . "/home/jon/Code/data-ethics-literature-review/notes")
+         (org-roam-db-location . "/home/jon/Code/data-ethics-literature-review/org-roam.db"))))
--- README.md
+++
@@ -1,2 +0,0 @@
-# data-ethics-literature-review
-A survey of literature surrounding ethics in data science.
