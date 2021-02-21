2c61fe2c5481dd5c4658583271cf6110a15bb5b3
Jonathan Reeve <jon.reeve@gmail.com>
2021-02-20 19:06:47 -0500

    add remaining files and move notes to public

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,2 @@
-((nil . ((org-roam-directory . "/home/jon/Code/data-ethics-literature-review/notes")
+((nil . ((org-roam-directory . "/home/jon/Code/data-ethics-literature-review/public")
          (org-roam-db-location . "/home/jon/Code/data-ethics-literature-review/org-roam.db"))))
