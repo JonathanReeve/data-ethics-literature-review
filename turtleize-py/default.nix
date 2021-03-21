5384b0e8cb4056b45d8ad67330dac3e6c783f09c
Jonathan Reeve <jon.reeve@gmail.com>
2021-03-21 11:52:54 -0400

    add readings from a few syllabi

--- turtleize-py/default.nix
+++ turtleize-py/default.nix
@@ -36,22 +36,71 @@ with import <nixpkgs> {};
       doCheck = false;
 
     };
-in pkgs.python3.buildEnv.override rec {
-    extraLibs = with pkgs.python3Packages; [
-	    matplotlib
-      pandas
-      jupyter
-      scikitlearn
-      newAltair
-      dominate
-      # plotly
-      newPlotly
-      # chart-studio
-      jupyterlab # Dev
-      # flake8  # Dev
-      # python-language-server
-      # pyls-mypy
-      toolz
-      rdflib
-    ];
-  }).env
+    requestsHtml = pkgs.python38Packages.buildPythonPackage rec { 
+	    pname = "requests-html"; 
+	    version = "0.10.0";
+	    src = pkgs.python38Packages.fetchPypi { 
+	      inherit version; inherit pname; 
+	      sha256 = "fpKez+2V+x0JlLs2gpXW18TQawP8uQDDPX0LF+YAOUc=";
+	    };
+	    propagatedBuildInputs = with pkgs.python38Packages; [ 
+	      pyquery
+	      requests
+	      fake-useragent
+	      parse
+	      beautifulsoup4
+	      pyppeteer
+	      networkx
+	      w3lib
+	    ];
+	    postPatch = ''
+	      substituteInPlace setup.py --replace "bs4" "beautifulsoup4"
+	    '';
+	    doCheck = false;
+    };
+    etudier = pkgs.python38Packages.buildPythonPackage rec { 
+	    pname = "etudier"; 
+	    version = "0.0.7";
+	    src = pkgs.python38Packages.fetchPypi { 
+	      inherit version; inherit pname; 
+	      sha256 = "0qy0h0bdlp1klzd3jjdky8l3z160cck0nlh5i3kgxgq1blsfkwb4";
+	    };
+	    buildInputs = with pkgs.python38Packages; [ 
+	      selenium
+	      networkx
+	      requestsHtml
+	    ];
+	    nativeBuildInputs = [ chromedriver ];
+	    doCheck = false;
+    }; 
+    customPython = pkgs.python38.buildEnv.override rec {
+	    extraLibs = with pkgs.python38Packages; [
+	      pkgs.chromedriver
+	      matplotlib
+	      pandas
+	      jupyter
+	      scikitlearn
+	      newAltair
+	      dominate
+	      # plotly
+	      newPlotly
+	      # chart-studio
+	      jupyterlab # Dev
+	      # flake8  # Dev
+	      # python-language-server
+	      # pyls-mypy
+	      toolz
+	      rdflib
+	      beautifulsoup4
+	      requestsHtml
+	      selenium
+	      etudier
+	      networkx
+        docker
+        click
+	    ];
+    };
+    in 
+    pkgs.mkShell { 
+      buildInputs = [ customPython chromedriver ];
+    })
--- turtleize-py/translate.py
+++ turtleize-py/translate.py
@@ -4,6 +4,8 @@ import requests
 from time import sleep
 import json
 import click
+import re
+from bs4 import BeautifulSoup
 
 """
 Use the Zotero Translation Server (https://github.com/zotero/translation-server)
@@ -28,7 +30,7 @@ def translateURL(url):
     """
     Get bibliographic information from a URL.
     """
-    print(f"Translating URL: {url}")
+    # print(f"Translating URL: {url}")
     response = requests.post("http://127.0.0.1:1969/web",
                             data=url,
                             headers={'Content-Type': 'text/plain'})
@@ -45,33 +47,135 @@ def translateJSON(zoteroJSON):
                              params={"format": "rdf_bibliontology"},
                              headers={"Content-Type": "application/json"})
     if response.ok:
-        print(response.text)
+        return response.text
 
-# zoteroJSON = translateURL('https://www.nytimes.com/2018/06/11/technology/net-neutrality-repeal.html')
-# decodedJSON = json.dumps(json.loads(zoteroJSON))
-# translateJSON(decodedJSON)
+def translateID(ident):
+    response = requests.post('http://127.0.0.1:1969/search',
+                             data=ident,
+                             params={"format": "rdf_bibliontology"},
+                             headers={"Content-Type": "text/plain"})
+    if response.ok:
+        return response.text
 
-def translateID():
-    pass
+def ident2rdf(ident):
+    """
+    Translate an identifier to RDF, where that identifier is one of a DOI, ISBN, arXiv ID, etc.
+    """
+    zoteroJSON = translateID(ident)
+    try:
+        decodedJSON = json.dumps(json.loads(zoteroJSON))
+    except:
+        print(f"Couldn't translate identifier: {ident}")
+        print(zoteroJSON)
+        return None
+    return translateJSON(decodedJSON)
+
+
+def url2rdf(url):
+    """ Converts a URL to RDF/XML. """
+    zoteroJSON = translateURL(url)
+    try:
+        decodedJSON = json.dumps(json.loads(zoteroJSON))
+    except:
+        print(f"Couldn't translate url: {url}")
+        return None
+    return translateJSON(decodedJSON)
+
+def getSyllabus(url):
+    resp = requests.get(url)
+    if resp.ok:
+        return resp.text
+    else:
+        exit("Response not OK.")
+
+def getURLs(html):
+    soup = BeautifulSoup(html)
+    links = soup.find_all('a')
+    return [link.get('href') for link in links]
+
+def processURLs(urls):
+    allItemIDs = []
+    if len(urls) > 0:
+        for url in urls:
+            print(f"Trying url: {url}")
+            rdf = url2rdf(url)
+            if rdf is not None:
+                itemId = writeRDF(rdf)
+                allItemIDs.append(itemId)
+    print(formatIDs(allItemIDs))
+
+def writeRDF(rdf):
+    matches = re.finditer('<z:UserItem rdf:about="(.*?)">', rdf)
+    itemIds = [match.group(1) for match in matches if match is not None]
+    itemId = itemIds[0]
+    fn = f"{itemId}.rdf.xml"
+    with open(fn, 'w') as f:
+        f.write(rdf)
+    print(f"Wrote {fn}")
+    click.echo(rdf)
+    return itemId
+
+def formatIDs(itemList):
+    """
+    We have lots of zotero ids, and we want to turn these into links in RDF.
+    """
+    return f"""
+    ccso:hasLM {" , ".join(itemList)} ;
+    """
+
+def getISBN(query):
+    params = {"q": query}
+    resp = requests.get('https://www.googleapis.com/books/v1/volumes', params=params)
+    if resp.ok:
+        data = json.loads(resp.text)
+        firstItem = data.get('items')[0]
+        isbns = [ident['identifier'] for ident in
+                 firstItem['volumeInfo']['industryIdentifiers']
+                 if ident['type']=='ISBN_13' or ident['type']=='ISBN_10']
+        return max(isbns)
+    else:
+        print("Something went wrong with this query.")
+        print(resp)
+
 
 @click.group()
 def cli():
-    click.echo('Heyo!')
+    pass
 
 @cli.command()
 @click.argument('URL', nargs=1)
 def url(url):
     """Translate a URL to RDF"""
-    zoteroJSON = translateURL(url)
-    decodedJSON = json.dumps(json.loads(zoteroJSON))
-    translateJSON(decodedJSON)
+    rdf = url2rdf(url)
+    writeRDF(rdf)
+    click.echo(rdf)
 
 
 @cli.command()
-@click.argument('URL', nargs=1)
-def identifier(url):
+@click.argument('ident', nargs=1)
+def identifier(ident):
     """Translate a DOI, arXiv, or ISBN to RDF"""
-    translateID()
+    click.echo(ident2rdf(ident))
+
+@cli.command()
+@click.argument('URL', nargs=1)
+def syllabus(url):
+    """Download a syllabus from a URL, extract links from it,
+    and create RDF from those links."""
+    html = getSyllabus(url)
+    links = getURLs(html)
+    processURLs(links)
+
+@cli.command()
+@click.argument('query', nargs=1)
+def book(query):
+    """ Query Google Books for a book, and
+    try to get its ISBN. Then, turn that into RDF."""
+    isbn = getISBN(query)
+    print('ISBN: ', isbn)
+    rdf = ident2rdf(isbn)
+    click.echo(rdf)
+    click.echo(writeRDF(rdf))
 
 if __name__== "__main__":
     cli()
