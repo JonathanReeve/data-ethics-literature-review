a047f846cd6c57a709c508ab2e2f11e162f1f6ff
Jonathan Reeve <jon.reeve@gmail.com>
2021-04-02 23:10:47 -0400

    first attempt at longer abstract

---
+++ aoir-abstract/build.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/env nix-shell
+#! nix-shell -i bash -p pandoc haskellPackages.pandoc-crossref
+
+# This script can be run with the command ./build.sh, and requires the Nix package manager.
+# Alternatively, install pandoc, pandoc-crossref, and run the command below.
+
+pandoc -f org+smart -o abstract.pdf abstract.org --bibliography=../bibliography.bib --filter=pandoc-crossref --citeproc
