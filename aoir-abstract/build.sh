5bb0e6b36142d4f55a742fd163ddf20847ec8c8f
Jonathan Reeve <jon.reeve@gmail.com>
2021-04-03 12:35:18 -0400

    rebuild and add link color

--- aoir-abstract/build.sh
+++ aoir-abstract/build.sh
@@ -4,4 +4,4 @@
 # This script can be run with the command ./build.sh, and requires the Nix package manager.
 # Alternatively, install pandoc, pandoc-crossref, and run the command below.
 
-pandoc -f org+smart -o abstract.pdf abstract.org --bibliography=../bibliography.bib --filter=pandoc-crossref --citeproc
+pandoc -f org+smart -o abstract.pdf abstract.org --bibliography=../bibliography.bib --filter=pandoc-crossref --citeproc -Vurlcolor=blue
